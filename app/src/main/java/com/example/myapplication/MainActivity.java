7866da5634bd8eeb333b630ba6294cc3595e8040
Andriy <ak8022@bs.amalnet.k12.il>
2020-10-04 16:38:23 +0300

    Corrected array

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,8 +16,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                           {"Armenia","Georgia","Hong Kong","Israel","Japan","Singapore","Taiwan"},
                           {"Albania","Belgium","Finland","France","Norway","Spain","United Kingdom"},
                           {"Anguila","Bermuda","Canada","Cayman Islands","Greenland","St. Martin","United States"},
-                          {"Argentina","Brazil","Chile","Peru","Uruguay","Falkland Islands","French Guiana"},
-                          {"Australia","French Polynesia","Guam","Marshall Islands","New Caledonia","New Zealand","Pitcairn Islands"}};
+                          {"Australia","French Polynesia","Guam","Marshall Islands","New Caledonia","New Zealand","Pitcairn Islands"},
+                          {"Argentina","Brazil","Chile","Peru","Uruguay","Falkland Islands","French Guiana"}};
 
     Spinner continetsSpinner;
     ListView countriesListView;
@@ -40,7 +40,16 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
     {
+        if (parent.getId()==continetsSpinner.getId())
+        {
+            if (position==0) return;
+            ArrayAdapter<String> adp=new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, countries[position-1]);
+            countriesListView.setAdapter(adp);
+        }
+        else
+        {
 
+        }
     }
 
     @Override
